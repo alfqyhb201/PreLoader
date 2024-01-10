e95adb85f118fd6646b7abdf4ad7edb009e566de
DarkEye450 <omr.jded@gmail.com>
2024-01-10 03:18:56 +0300

    Add bg

--- js/gsap.js
+++ js/gsap.js
@@ -7,6 +7,13 @@ function hideLoader() {
         duration: 2,
         // delay: 2,
         y: -2000,
+        opacity: 0,
+        onComplete: () => {
+            console.log('loader hidden');
+            window.scrollTo({ top: 0 });
+            // showContent();
+            colorWrapper.remove();
+            }
         // opacity: 0,
         // ease: "bounce",
     });
@@ -14,9 +21,9 @@ function hideLoader() {
     //     duration: 2,
     //     delay: 2
     // });
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-      }
+    // function sleep(ms) {
+    //     return new Promise(resolve => setTimeout(resolve, ms));
+    //   }
       
-      sleep(2000).then(() => { colorWrapper.remove();  });
+    //   sleep(2000).then(() => { colorWrapper.remove();  });
 }
