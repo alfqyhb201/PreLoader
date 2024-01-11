904320ff25a03df98d40d4121904b8c3badc6ee3
DarkEye450 <omr.jded@gmail.com>
2024-01-11 22:03:53 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let num = 0;
 let red = 0, green = 0;
 const loaderElement = document.querySelector('.loader');
-let timer = setInterval(loader, 200);
+let timer = setInterval(loader, 20);
 function loader() {
     if (num < 100) {
         num++;
